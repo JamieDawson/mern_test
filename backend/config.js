9307ff695800cced57c601b17d6af6be86259b21
Jamie Dawson <jamierobertdawson@gmail.com>
2024-05-30 21:07:11 -0700

    created models folder

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,8 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://jamierobertdawson:Wp6lUnhBYNBKTfAw@books-store-mern.cyuxqtu.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN";
+
+//Please create a free database for yourself
+
+//this database will be deleted after tutorial
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { PORT } from "./config.js";
-
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`PORT IS ON ${PORT}`);
-});
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to the app");
+}); //the root
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("connected to mongodb database!!!");
+    app.listen(PORT, () => {
+      console.log(`PORT IS ON ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
